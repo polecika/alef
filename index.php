3d72db993ddce403ee40618dd03383983e111ced
Polina Svet <polecika@mail.ru>
2021-10-21 23:24:38 +0300

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Задание от Alef Development</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+  <h1>Задание 1</h1>
+  <h1>Задание 2</h1>
+  <h1>Задание 3</h1>
+</body>
+</html>
